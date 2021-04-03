8f5bbf1a755df49a8ada26e7410e17289615791d
Roy Sizemore <rsizemore@swampcastle.home>
2021-04-03 11:08:28 -0400

    Added Phase II Backend - basic express server listening functionality

---
+++ data/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const router = require('express').Router();
+
+
+// Assigning express() to a variable to streamline code
+const app = express();
+const port = process.env.port || 6101;
+
+// Data parsing/handling
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
+
