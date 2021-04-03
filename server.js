03656f3d5a7ea8f7baf4f10e0b2a67f5fd8ce575
Roy Sizemore <rsizemore@swampcastle.home>
2021-04-03 12:40:38 -0400

    Added comment and cleaned up whitespace in server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const router = require('./routes/routes');
 
-
 // Assigning express() to a variable to streamline code
 const app = express();
 const port = process.env.port || 6101;
@@ -13,5 +12,6 @@ app.use(express.json());
 
 app.use('/', router);
 
+// Starts express server listener
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
 
